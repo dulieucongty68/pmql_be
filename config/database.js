2ca24a09b00d1546b2757c9175d3f306dc01208b
Duchnh <hduy01012000@gmail.com>
2025-03-05 20:18:14 +0700

    add log

--- config/database.js
+++ config/database.js
@@ -15,6 +15,7 @@ const connectDB = async () => {
   try {
     await sequelize.authenticate();
     console.log("✅ Database connected successfully.");
+    console.log(process.env.DATABASE_URL);
   } catch (error) {
     console.error("❌ Database connection failed:", error);
   }
